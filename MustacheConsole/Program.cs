adce27e62b2e3a0b76b4033298f3f486b234ac47
Yuval Greenfield <ubershmekel@gmail.com>
2015-05-17 22:39:23 -0700

    Initial console app commit

---
+++ MustacheConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MustacheCs;
+
+namespace MustacheConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mustache.render("asdf{{!ignoredcomment...}} me", null));
+        }
+    }
+}
